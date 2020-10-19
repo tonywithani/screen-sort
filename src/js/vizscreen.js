da76c8bfdef2da9e53c27aa2aee199e76ea644a7
tonywithani <32919888+tonywithani@users.noreply.github.com>
2020-10-19 16:22:48 +1100

    Initialise

---
+++ src/js/vizscreen.js
@@ -0,0 +1,35 @@
+class VizScreens {
+    constructor() {
+        this.dest = dest;
+        this.src = src;
+    }
+
+    get destination() {
+        console.log(this.dest);
+        return this.dest;
+    }
+
+    get source() {
+        console.log(this.src);
+        return this.src;
+    }
+
+    // Check if directory exists
+    ckdir() {
+        return;
+    }
+
+    // Make directories.
+    mkdir() {
+        return;
+    }
+
+    // Remove files.
+    rm() {
+        return;
+    }
+
+    extract() {
+        return;
+    }
+}
