4e7c7ac56d38fd869a0dd00529c27c4ebc8039c3
tonywithani <32919888+tonywithani@users.noreply.github.com>
2020-10-26 10:59:16 +1100

    v0.1.20 - VS Code Github Test

--- src/vizscreens.js
+++ src/vizscreens.js
@@ -3,46 +3,6 @@ class VizScreens {
         this.dest = null;
         this.src = null;
     }
-
-    set destination(arg) {
-        this.dest = arg;
-    }
-
-    get destination() {
-        return this.dest;
-    }
-
-    set source(arg) {
-        this.src = arg;
-    }
-
-    get source() {
-        return this.src;
-    }
-
-    // // Check if directory exists
-    // ckdir() {
-    //     return;
-    // }
-
-    // // Make directories.
-    // mkdir() {
-    //     return;
-    // }
-
-    // // Remove files.
-    // rm() {
-    //     return;
-    // }
-
-    // extract() {
-    //     return;
-    // }
-
-    resest() {
-        this.dest = null;
-        this.src = null;
-    }
 }
 
 module.exports = VizScreens
